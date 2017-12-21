3b0400cccc2047904e754ff484a413e66829759f
杨海 <632428109@qq.com>
2017-12-21 17:01:30 +0800

    6

--- src/test/java/com/hai/shiro/LoginLogoutTest.java
+++ src/test/java/com/hai/shiro/LoginLogoutTest.java
@@ -1,7 +1,6 @@
 package com.hai.shiro;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.subject.Subject;
@@ -18,10 +17,9 @@ public class LoginLogoutTest {
 		SecurityUtils.setSecurityManager(securityManager);
 		Subject subject = SecurityUtils.getSubject();
 		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
-			subject.login(token);
-		} catch (AuthenticationException e) {
-			e.printStackTrace();
-		}
+		subject.login(token);
+		//5555555
+	
 		Assert.assertEquals(true, subject.isAuthenticated());
 		//2018 11111
 		subject.logout();
