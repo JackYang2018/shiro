08f2eec54f2e867d85d1a3da574d6cb2077bda26
JackYang2018 <34700696+JackYang2018@users.noreply.github.com>
2017-12-21 16:46:07 +0800

    Update LoginLogoutTest.java

--- src/test/java/com/hai/shiro/LoginLogoutTest.java
+++ src/test/java/com/hai/shiro/LoginLogoutTest.java
@@ -16,18 +16,13 @@ public class LoginLogoutTest {
 		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("F:/sts_workspace/shiro/src/main/resources/shiro.ini");
 		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
 		SecurityUtils.setSecurityManager(securityManager);
-		
 		Subject subject = SecurityUtils.getSubject();
 		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
-		//19999 98888888 99999999
-		try {
 			subject.login(token);
 		} catch (AuthenticationException e) {
 			e.printStackTrace();
 		}
-	        //2017
 		Assert.assertEquals(true, subject.isAuthenticated()); //¶ÏÑÔÓÃ»§ÒÑ¾­µÇÂ¼
-		
 		subject.logout();
 	}
 
