23961e8cf89ddc94798179f5fda6aa068d6b0191
杨海 <632428109@qq.com>
2017-12-21 16:14:56 +0800

    git新项目

---
+++ src/test/java/com/hai/shiro/LoginLogoutTest.java
@@ -0,0 +1,34 @@
+package com.hai.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginLogoutTest {
+	
+	@Test
+	public void testHelloworld() {
+		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("F:/sts_workspace/shiro/src/main/resources/shiro.ini");
+		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+		
+		try {
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}
+		
+		Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+		
+		subject.logout();
+	}
+
+}
