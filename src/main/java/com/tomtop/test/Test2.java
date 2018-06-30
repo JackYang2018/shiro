c1056dd0da95fa027b9b2e5bcfad41ac2d449032
杨海 <632428109@qq.com>
2018-06-30 10:58:33 +0800

    update test and test2

--- src/main/java/com/tomtop/test/Test2.java
+++ src/main/java/com/tomtop/test/Test2.java
@@ -1,7 +1,9 @@
 package com.tomtop.test;
 
 public class Test2 {
-  public static void main(String[] args) {
+	
+    public static void main(String[] args) {
 		System.out.println("hello yanghai");
 	}
+	
 }
