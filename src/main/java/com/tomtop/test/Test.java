1e9be8a61f7a9d0355f3ee9acb29ebcfefd8efe8
JackYang2018 <34700696+JackYang2018@users.noreply.github.com>
2018-06-30 10:32:31 +0800

    Update Test.java

--- src/main/java/com/tomtop/test/Test.java
+++ src/main/java/com/tomtop/test/Test.java
@@ -3,8 +3,6 @@ package com.tomtop.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello test");
-		System.out.println("hello world");
 		System.out.println("hello yanghai");
 	}
 
