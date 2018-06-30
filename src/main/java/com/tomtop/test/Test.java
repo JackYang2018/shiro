39a9eb6250d070e853f741991b0972170f01893b
JackYang2018 <34700696+JackYang2018@users.noreply.github.com>
2018-06-30 11:00:46 +0800

    Update Test.java

--- src/main/java/com/tomtop/test/Test.java
+++ src/main/java/com/tomtop/test/Test.java
@@ -3,8 +3,15 @@ package com.tomtop.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello yanghai");
-		System.out.println("hello world");
+		System.out.println("hello yangliu");
+		System.out.println("hello world3");
+		
+		
+		
+		
+		System.out.println("hello a");
+		System.out.println("hello b");
+		System.out.println("hello c");
 	}
 
 }
